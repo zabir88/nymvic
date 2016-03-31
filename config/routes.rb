8126221298fd63670a075d252747afa0ed54f06f
Zabir <zabir8809@gmail.com>
2016-03-31 18:04:22 -0400

    update navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:index]
   root 'users#index'
   get "/pages/:about" => "pages#about" 
   get "/pages/:contact" => "pages#contact"
