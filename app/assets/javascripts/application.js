5198ca88da215c809c50df1f2a8e24dc380f70dc
Zabir <zabir8809@gmail.com>
2016-04-03 17:03:44 -0400

    fixed drop down for navbar for closing when item is clicked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,8 +42,10 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-    $('.navbar-toggle:visible').click();
-  }
+$(function(){
+    $('.navbar-collapse ul li a').click(function() {
+        if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+            $('.navbar-toggle:visible').click();
+         }
+    });
 });
