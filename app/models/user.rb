971964378f8679132abd1263e861fb21e3a09d4c
Zabir <zabir8809@gmail.com>
2016-04-05 09:29:41 -0400

    added dropdowns in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	validates :first_name, :last_name, :birth_year, :gender, :marital_status, :children, :party_affliation, :email, :telephone, presence: true 
+	validates :first_name, :last_name, :birth_year, :email, :telephone, presence: true
+	validates :gender, :marital_status, :children, :party_affliation, presence: true, allow_blank: false  
 	validates :birth_year, :telephone, numericality: {only_integer: true, message: "Enter numbers only"}
 	validates :email, format: { with:  /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i, message: "Enter a valid email" }
 	validates_uniqueness_of :email, message: 'Email has already been taken'
