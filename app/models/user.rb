d60826a383cf8fd3ae02f7575cb4d6846a77b8e2
Zabir <zabir8809@gmail.com>
2016-04-05 14:54:48 -0400

    integrate mailchimp api

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,9 @@ class User < ActiveRecord::Base
 	validates :email, format: { with:  /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i, message: "Enter a valid email" }
 	validates_uniqueness_of :email, message: 'Email has already been taken'
 
+	after_create :subscribe
+	
 	def subscribe
-    mailchimp = Gibbon::Request.new(api_key: ENV['mailchimp_api_key'])
-    list_id = ENV['mailchimp_list_id']
-    result = mailchimp.lists(list_id).members.create(
-      body: {
-        email_address: self.email,
-        status: 'subscribed',
-        merge_fields: {
-          FNAME: self.full_name,
-          LNAME: self.last_name,
-          BYEAR: self.birth_year,
-          GEND: self.gender,
-          MSTAT: self.marital_status,
-          CHILDREN: self.children,
-          PARTY: self.party_affliation,
-          TELPHONE: self.telephone
-      	}
-    })
-    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
-  end
-
+    	MailingListSignupJob.perform_later(self)
+  	end
 end
---
+++ config/initializers/active_job.rb
@@ -0,0 +1,3 @@
+require 'sucker_punch/async_syntax'
+#ActiveJob::Base.queue_adapter = :inline
+ActiveJob::Base.queue_adapter = :sucker_punch
