755d75d42510eaf79ac2038430251a1a98145d73
Zabir <zabir8809@gmail.com>
2016-04-23 14:08:01 -0400

    removed aws-ses gem and manually configured email in production

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,9 @@
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: ENV['AWS_ACCESS_KEY'],
-  secret_access_key: ENV['AWS_SECRET_KEY'],
-  server: 'email.us-west-2.amazonaws.com'
+require 'net/smtp'
+
+module Net  
+  class SMTP
+    def tls?
+      true
+    end
+  end
+end  
