29b04ab0e4b5c450103c4f577f66cc1ba8082a04
Zabir <zabir8809@gmail.com>
2016-04-23 13:45:55 -0400

    testing crontab

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   access_key_id: ENV['AWS_ACCESS_KEY'],
   secret_access_key: ENV['AWS_SECRET_KEY'],
-  :server=> 'email.us-west-2.amazonaws.com'
+  server: 'email.us-west-2.amazonaws.com'
