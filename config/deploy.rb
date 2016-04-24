9749af6db38c0b91c98ca0288858c0fca1388d98
Zabir <zabir8809@gmail.com>
2016-04-24 15:50:21 -0400

    changed production assets configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, '2.2.3' # Edit this if you are using MRI Ruby
-set :assets_prefix, "/shared/public/assets"
+set :assets_prefix, "shared/public/assets"
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.assets.prefix = "/shared/public/assets"
+  config.assets.prefix = "shared/public/assets"
   
   config.action_mailer.delivery_method = :ses
   
