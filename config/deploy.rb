3326491e30f28b5e614371903c806cdf52bfeb1e
Zabir <zabir8809@gmail.com>
2016-04-24 17:27:21 -0400

    changed deploy.rb to include assets prefix

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, '2.2.3' # Edit this if you are using MRI Ruby
-set :assets_prefix, "shared/public/assets" 
+set :rvm_ruby_version, '2.2.3' # Edit this if you are using MRI Ruby 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
