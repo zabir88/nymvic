01306f2a1acf35748e68c53bbff0090ca1b35960
Zabir <zabir8809@gmail.com>
2016-04-24 20:38:39 -0400

    assets compiled for production

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module Nymvic
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.active_job.queue_adapter = :delayed_job    
+    config.active_job.queue_adapter = :delayed_job 
+    config.assets.initialize_on_precompile = false
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
