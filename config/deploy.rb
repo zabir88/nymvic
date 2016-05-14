1523867c5f2880dfc7dbd1888b57a8a80049acd6
Zabir <zabir8809@gmail.com>
2016-05-14 01:32:28 -0400

    change in config/deploy.rb to add databse seed task

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,4 +41,13 @@ namespace :deploy do
     invoke 'delayed_job:restart'
   end
 end
-
+desc 'Runs rake db:seed'
+task :seed => [:set_rails_env] do
+  on primary fetch(:migration_role) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
+      end
+    end
+  end
+end
