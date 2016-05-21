b3b5300095cb3e43f405968880f1771673cd821f
Zabir <zabir8809@gmail.com>
2016-05-21 08:30:39 -0400

    fresh try for seeding production database using roo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'roo'
 require 'resolv'
 
-open_data= Roo::Excelx.new('db/nymvicdata.xlsx')
+open_data= Roo::Excelx.new(Rails.root.join('db', 'nymvicdata.xlsx'))
 parsed_data= open_data.parse(email: 'email', first_name: 'first_name', last_name: 'last_name')
 
 def valid_email_domain(email)
