f74c9f242d4f9a381bc51321910dc90e041d1fdf
Zabir <zabir8809@gmail.com>
2016-05-22 13:08:40 -0400

    fixed reminder email job

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -2,11 +2,8 @@ class SendEmailJob < ActiveJob::Base
   queue_as :emailreminder
 
   def perform
-  	User.find_each(batch_size: 5000) do |u|
-  		if u.select{|i| i.email.match(/\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/)}
-  			
-  		end
-  		#EmailUsers.reminder_email(u).deliver_now
+  	User.find_each(start: 459, batch_size: 2283) do |u|
+		EmailUsers.reminder_email(u).deliver_now
   	end
   end
 end
