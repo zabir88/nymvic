3f350a2453917808cbc75f629af3bb3e3712b60f
zabir88 <zabir8809@gmail.com>
2016-10-08 20:15:09 -0400

    cronjob updated

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -2,7 +2,7 @@ class SendEmailJob < ActiveJob::Base
   queue_as :emailreminder
 
   def perform
-  	User.find_each(start: 459, batch_size: 2283) do |u|
+  	User.find_each(start: 1, batch_size: 1) do |u|
 		EmailUsers.reminder_email(u).deliver_now
   	end
   end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,14 @@
 # Example:
 #set :environment, "development"
-set :environment, "production"
+#set :environment, "production"
 set :output, {:error=> "log/cron_error_log.log", :standard=> 'log/cron_log.log'}
 
 =begin
 every :sunday, :at=> '7:16 pm' do
 	runner "SendSmsJob.perform_later"
 end
-=end
-every :saturday, :at=> '6:15 pm' do
+
+every :saturday, :at=> '8:08 pm' do
 	runner "SendEmailJob.perform_later"
 end
+=end
