757d9701a0e654a55469cfe97df90fffdef2314d
zabir88 <zabir8809@gmail.com>
2016-11-14 23:02:23 -0500

    cahnged schedule email reminder

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,17 @@
 # Example:
 #set :environment, "development"
-set :environment, "production"
-set :output, {:error=> "log/cron_error_log.log", :standard=> 'log/cron_log.log'}
+#set :environment, "production"
+#set :output, {:error=> "log/cron_error_log.log", :standard=> 'log/cron_log.log'}
 
 =begin
 every :sunday, :at=> '7:16 pm' do
 	runner "SendSmsJob.perform_later"
 end
-=end
+
 
 every :monday, :at=> '10:40 pm' do
 	runner "SendEmailJob.perform_later"
 end
-
+=end
 
 #run this to update the crontab whenever --update-crontab
